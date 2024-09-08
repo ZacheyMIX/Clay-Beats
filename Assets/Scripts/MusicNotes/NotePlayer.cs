8f37045dd0a0e318cace06436b11c3edac791d32
Hayes Geldmacher <hayes.g850@gmail.com>
2024-09-08 14:28:30 -0600

    Starting JSON reader

--- Assets/Scripts/MusicNotes/NotePlayer.cs
+++ Assets/Scripts/MusicNotes/NotePlayer.cs
@@ -1,17 +1,33 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NotePlayer : MonoBehaviour
 {
-
+    private string _jsonString;
     private NoteManager _noteManager;
     
     // Start is called before the first frame update
     void Start()
     {
         _noteManager = GetComponent<NoteManager>();
+
+       // SongData _songData = new SongData();
+       // _songData._timeSignature = 0.75f;
+       // _songData._duration = 30f;
+       // _songData._bpm = 100;
+
+      //  string json = JsonUtility.ToJson(_songData);
+       // Debug.Log(json);
+
+      //  SongData _loadedSongData = JsonUtility.FromJson<SongData>(json);
+      //  Debug.Log(_loadedSongData);
+
+      _jsonString = File.ReadAllText(Application.dataPath + "/Scripts/JSON/Test1.json");
+        Debug.Log(_jsonString);
     }
+
 
     // Update is called once per frame
     void Update()
@@ -23,7 +39,12 @@ public class NotePlayer : MonoBehaviour
     }
 
 
-
+    private class SongData
+    {
+        public float _timeSignature;
+        public float _duration;
+        public float _bpm;
+    }
     //template for calling note function! 
     //(bool _left, string _noteKey, int _spawnerKey)
 
