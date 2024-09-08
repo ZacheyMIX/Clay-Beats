7cc1caabedecf39881c0214c7c52b48c20119af5
Hayes Geldmacher <hayes.g850@gmail.com>
2024-09-08 11:59:04 -0600

    Adding dictionary for note objects to spawn

--- Assets/Scripts/MusicNotes/NoteManager.cs
+++ Assets/Scripts/MusicNotes/NoteManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NoteManager : MonoBehaviour
 {
+
+    //This List contains all of the note objects that are then stored in the below dictionary
+    [SerializeField] private GameObject[] _notesPool;
+
+    [SerializeField] private string[] _stringsPool;
+
     //This dictionary carries all of the possible notes that could be spawned
     [SerializeField] private Dictionary<string, GameObject> _noteObjects = new Dictionary<string, GameObject>();
 
@@ -13,13 +19,13 @@ public class NoteManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       //forloop collects all string and gameobject data to form a dictionary for notes  
+        int i = 0;
+        foreach(GameObject note in _notesPool)
+        {
+            _noteObjects.Add(_stringsPool[i], note);
+            i++;
+        }
     }
 
     private void SpawnNote(string _noteKey, int _spawnerKey)
